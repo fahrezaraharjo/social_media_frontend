9ce1506d97917a75467a460c600f70006a182445
fahreza raharjo <fahrezaraharjo92@gmail.com>
2023-01-08 14:24:49 +0700

    server  auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import ProfilePage from "scenes/profilepage";
+import LoginPage from "scenes/loginpage";
 
 function App() {
+
+
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
